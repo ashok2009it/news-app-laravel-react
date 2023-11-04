96efbc67debdbba012630f989fa9d9cf05465fb6
Ashok <ashok2009it@gmail.com>
2023-11-04 15:03:43 +0530

    update build

--- resources/js/components/footer/Footer.jsx
+++ resources/js/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} NewsApp. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} NewsApp By Ashok. All rights reserved.</p>
     </footer>
   );
 };
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html> <html lang="en">
 <head>
+<title>Welcome to the take-home challenge</title>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    <div id="app"></div> <!-- This is where your React app will mount -->
-
-    <!-- Other content -->
-
+    <div id="app"></div>
     <!-- Include your JavaScript file using Laravel Vite -->
     @viteReactRefresh
     @vite(['resources/js/app.tsx'])
 </body>
+
 </html>
